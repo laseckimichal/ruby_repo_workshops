1edddee7a91994774c9ccf3edf2986fb96826b1b
Mike <michal.lasecki@firma.interia.pl>
2023-10-03 15:18:09 +0200

    feat: add password validator

--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,20 @@
 class ValidatePassword
+  attr_reader :password
 
+  def initialize(password)
+    @password = password
+  end
+
+  def perform
+    password_conditions = [
+      (6..24).cover?(password.length),
+      password =~ /[A-Z]/,
+      password =~ /[a-z]/,
+      password =~ /\d/,
+      password =~ /[!@#\$%&\*\+=:;?<>]/,
+      password !~ /(.)\1{2,}/
+    ]
+
+    password_conditions.all?
+  end
 end
