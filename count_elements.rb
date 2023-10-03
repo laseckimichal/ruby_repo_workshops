6b2bcb813d9d69da61fc9bf73d38b6ed4c2ab370
Mike <michal.lasecki@firma.interia.pl>
2023-10-03 15:17:56 +0200

    feat: add element counter

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,12 @@
 class CountElements
+  attr_reader :args
 
+  def initialize(args)
+    @args = args
+  end
+
+  def perform
+    args.tally
+    # args.each_with_object(Hash.new(0)) { |w, counter| counter[w] += 1 } - can be used with ruby < 2.7
+  end
 end
